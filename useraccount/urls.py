9659efa8040e9347957c1d8c52e408769e0b842b
Ateeq ur Rehman <ateeqmughal272@gmail.com>
2021-10-05 00:41:23 +0500

    added link with pk search

--- useraccount/urls.py
+++ useraccount/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from .views import all_user
+from .views import all_user, specific_user
 # from .views import AllUserView
 
 urlpatterns = [
     path('', include('rest_auth.urls')),
     path('registration/', include('rest_auth.registration.urls')),
-    path('all-users/', all_user),
+    path('all-users', all_user),
+    path('all-users/<int:pk>', specific_user),
+
 
 ]
--- useraccount/views.py
+++ useraccount/views.py
@@ -1,11 +1,24 @@
-from allauth.account.models import EmailAddress
+from django.contrib.auth.models import User
 from rest_framework.response import Response
+from django.shortcuts import HttpResponse
 from rest_framework.decorators import api_view
+from django.core import serializers
 
 # # Create your views here.
 
 @api_view(('GET',))
 def all_user(request):
     
-    users = EmailAddress.objects.all().values()
+    users = User.objects.all().values()    
     return Response(users)
+
+
+@api_view(('GET',))
+def specific_user(request, pk):
+    
+    users = User.objects.get(id=pk)
+    response = {
+        "username" : users.username,
+        'email' : users.email
+    }
+    return Response(response)
